dbb01c6990d302a1ec86395d208265d640e7694c
ls123456789 <2953757467@qq.com>
2015-11-18 20:58:59 +0800

    关于多重派生的构造函数
    定义两个类，一个Teacher类和一个Student
    类，将两个类多重继承为一个Graduate类，并且给Graduate类做初始化。其中出现几处问题。strcpy不能使用，构造函数中的问题和Gra
    duate类对象的定义。

--- inheritance/main.cpp
+++ inheritance/main.cpp
@@ -7,9 +7,72 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
+class Teacher
+{
+public:
+    Teacher (string nam,int a,string t)
+    {
+        name = nam;
+        age = a;
+        title = t;
+    }
+    void display()
+    {
+        cout<<"name:"<<name<<endl;
+        cout<<"age:"<<age<<endl;
+        cout<<"title:"<<title<<endl;
+    }
+protected:
+    string name;
+    int age;
+    string title;
+};
+
+class Student
+{
+public:
+    Student(char nam[],char s,float sco)
+    {
+        strcpy(name1, nam);
+        sex = s;
+        score = sco;
+    }
+    void display1()
+    {
+        cout<<"name:"<<name1<<endl;
+        cout<<"name:"<<sex<<endl;
+        cout<<"name:"<<score<<endl;
+    }
+protected:
+    string name1;
+    char sex;
+    float score;
+};
+
+class Graduate:public Teacher,public Student
+{
+public:
+    Graduate(string nam,int a ,char s,string t, float sco,float w):
+        Teacher(nam,a,t),Student(nam,s,sco),wage(w){}
+    void show()
+    {
+        cout<<"name:"<<name<<endl;
+        cout<<"age:"<<age<<endl;
+        cout<<"sex:"<<sex<<endl;
+        cout<<"score:"<<score<<endl;
+        cout<<"title:"<<title<<endl;
+        cout<<"wage:"<<wage<<endl;
+    }
+private:
+    float wage;
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    Graduate gradl("wang", "24", "f", "assistant", "89.5", "2400");
+    gradl.show();
     return 0;
 }
